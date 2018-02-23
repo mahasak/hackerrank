81bb9355934d061cefc244d75505c0ebe47f42a4
Mahasak Pijittum <mahasak@gmail.com>
2018-02-23 22:24:31 +0700

    add prime

---
+++ prime.py
@@ -0,0 +1,19 @@
+def is_prime(n):
+    if n < 2: 
+         return False;
+    if n % 2 == 0:             
+         return n == 2  # return False
+    k = 3
+    while k*k <= n:
+         if n % k == 0:
+             return False
+         k += 2
+    return True
+
+p = int(input().strip())
+for a0 in range(p):
+    n = int(input().strip())
+    if is_prime(n):
+        print("Prime")
+    else:
+        print("Not prime")
